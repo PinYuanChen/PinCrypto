18b340724adfeef06a90ca7feac725623b092a54
Patrick <champion790713@gmail.com>
2022-12-21 15:24:30 +0800

    initial commit

---
+++ PinCrypto/PinCryptoApp.swift
@@ -0,0 +1,14 @@
+//
+// Created on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PinCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
