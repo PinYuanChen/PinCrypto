12cb66d2d9b8c60e936cccb3ead22d951a61b7b2
Patrick <champion790713@gmail.com>
2022-12-22 15:24:00 +0800

    [Home VM] bind coin data service to home vm

--- PinCrypto/Services/CoinDataService.swift
+++ PinCrypto/Services/CoinDataService.swift
@@ -20,7 +20,7 @@ class CoinDataService {
         
         coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
+            .tryMap { output -> Data in
                 guard let response = output.response as? HTTPURLResponse,
                       response.statusCode >= 200 && response.statusCode < 300 else {
                     throw URLError(.badServerResponse)
