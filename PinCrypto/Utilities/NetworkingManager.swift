82f06525cf595fc695d45767b9214df6d61cf8d6
Patrick <champion790713@gmail.com>
2022-12-22 15:34:32 +0800

    [Utilities] create networking manager

---
+++ PinCrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,25 @@
+//
+// Created on 2022/12/22.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+}
