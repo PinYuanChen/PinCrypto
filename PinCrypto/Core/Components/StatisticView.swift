c87cc91e55d9c5e0f4892fdc085bd4881518e6c0
Patrick <champion790713@gmail.com>
2022-12-23 15:38:45 +0800

    [Statistic] create model and view

---
+++ PinCrypto/Core/Components/StatisticView.swift
@@ -0,0 +1,17 @@
+//
+// Created on 2022/12/23.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticView()
+    }
+}
