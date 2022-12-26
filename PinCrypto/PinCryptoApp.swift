c5d36e19495cec93bd2eb7f1aa0ccd242d29a7f5
Patrick <champion790713@gmail.com>
2022-12-26 16:15:45 +0800

    [Nav] override navigation title color

--- PinCrypto/PinCryptoApp.swift
+++ PinCrypto/PinCryptoApp.swift
@@ -9,6 +9,11 @@ struct PinCryptoApp: App {
     
     @StateObject private var vm = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
