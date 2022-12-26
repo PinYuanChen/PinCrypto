8c07fee523b0cb5689dd82a0361c1a581938ca01
Patrick <champion790713@gmail.com>
2022-12-26 15:59:04 +0800

    [Portfolio] add input section

--- PinCrypto/Core/Home/Views/PortfolioView.swift
+++ PinCrypto/Core/Home/Views/PortfolioView.swift
@@ -8,22 +8,13 @@ struct PortfolioView: View {
     
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText = ""
     
     var body: some View {
         NavigationView {
             ScrollView {
-                VStack(alignment: .leading, spacing: 0) {
-                    SearchBarView(searchText: $vm.searchText)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack(spacing: 10) {
-                            ForEach(vm.allCoins) { coin in
-                                CoinLogoView(coin: coin)
-                                    .frame(width: 75)
-                            }
-                        }
-                    }
-                }
+                portfolioInputSection
             }
             .navigationTitle("Edit Portfolio")
             .toolbar(content: {
@@ -39,5 +30,71 @@ struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
             .environmentObject(dev.homeVM)
+    }
+}
+
+private extension PortfolioView {
+    var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke( selectedCoin?.id == coin.id ? Color.theme.green : .clear, lineWidth: 1)
+                        )
+                }
+            }
+            .frame(height: 120)
+            .padding(.leading)
+        }
+    }
+    
+    func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
+    }
+    
+    var portfolioInputSection: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            SearchBarView(searchText: $vm.searchText)
+            coinLogoList
+            
+            if selectedCoin != nil {
+                VStack(spacing: 20) {
+                    HStack {
+                        Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                        Spacer()
+                        Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+                    }
+                    Divider()
+                    HStack {
+                        Text("Amount holding:")
+                        Spacer()
+                        TextField("Ex: 1.4", text: $quantityText)
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.decimalPad)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Current value:")
+                        Spacer()
+                        Text(getCurrentValue().asCurrencyWith2Decimals())
+                    }
+                }
+                .animation(.none, value: selectedCoin?.id)
+                .padding()
+                .font(.headline)
+            }
+        }
     }
 }
