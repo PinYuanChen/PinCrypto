28744f06ec72aaf2405f11232d65f40793233edf
Patrick <champion790713@gmail.com>
2022-12-27 10:43:59 +0800

    [Service] add CRUD to portfolio service

--- PinCrypto/Services/PortfolioDataService.swift
+++ PinCrypto/Services/PortfolioDataService.swift
@@ -19,10 +19,25 @@ class PortfolioDataService {
             if let error = error {
                 print("Error loading Core Data! \(error)")
             }
+            self.getPortfolio()
+        }
+    }
+    
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        // check if coin is already in portfolio
+        if let entity = savedEntities.first(where: { $0.coinID == coin.id }) {
+            if amount > 0 {
+                update(entity: entity, amount: amount)
+            } else {
+                delete(entity: entity)
+            }
+        } else {
+            add(coin: coin, amount: amount)
         }
     }
 }
 
+// MARK: - Private functions
 private extension PortfolioDataService {
     func getPortfolio() {
         let request = NSFetchRequest<PortfolioEntity>(entityName: entityName)
@@ -32,5 +47,36 @@ private extension PortfolioDataService {
         } catch let error {
             print("Error fetching Portfolio Entities. \(error)")
         }
+    }
+    
+    func add(coin: CoinModel, amount: Double) {
+        // convert coin model to entity
+        let entity = PortfolioEntity(context: contanier.viewContext)
+        entity.coinID = coin.id
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    func update(entity: PortfolioEntity, amount: Double) {
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    func delete(entity: PortfolioEntity) {
+        contanier.viewContext.delete(entity)
+        applyChanges()
+    }
+    
+    func save() {
+        do {
+            try contanier.viewContext.save()
+        } catch let error {
+            print("Error saving to Core Data. \(error)")
+        }
+    }
+    
+    func applyChanges() {
+        save()
+        getPortfolio()
     }
 }
