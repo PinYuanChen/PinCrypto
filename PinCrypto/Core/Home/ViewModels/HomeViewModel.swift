d1322c571689601100969e782c798a4ede096877
Patrick <champion790713@gmail.com>
2022-12-27 14:53:56 +0800

    [Utilities] add haptic manager

--- PinCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ PinCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -63,6 +63,7 @@ class HomeViewModel: ObservableObject {
         isLoading = true
         coinDataService.getCoins()
         marketDataService.getData()
+        HapticManager.notification(type: .success)
     }
 }
 
---
+++ PinCrypto/Utilities/HapticManager.swift
@@ -0,0 +1,15 @@
+//
+// Created on 2022/12/27.
+//
+
+import Foundation
+import SwiftUI
+
+class HapticManager {
+    
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        generator.notificationOccurred(type)
+    }
+}
