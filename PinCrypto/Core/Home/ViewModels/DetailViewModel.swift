e9b95480fe73d6a699b4f181ca240ecd672bc20b
Patrick <champion790713@gmail.com>
2022-12-28 17:21:54 +0800

    [Detail] create service and model

---
+++ PinCrypto/Core/Home/ViewModels/DetailViewModel.swift
@@ -0,0 +1,28 @@
+//
+// Created on 2022/12/28.
+//
+
+import Foundation
+import Combine
+
+class DetailViewModel {
+    
+    init(coin: CoinModel) {
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private let coinDetailService: CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
+}
+
+private extension DetailViewModel {
+    func addSubscribers() {
+        coinDetailService
+            .$coinDetail
+            .sink { returnedCoinDetail in
+                print(returnedCoinDetail)
+            }
+            .store(in: &cancellables)
+    }
+}
---
+++ PinCrypto/Services/CoinDetailDataService.swift
@@ -0,0 +1,34 @@
+//
+// Created on 2022/12/28.
+//
+
+import Foundation
+import Combine
+
+class CoinDetailDataService {
+    
+    @Published var coinDetail: CoinDetailModel? = nil
+    var coinDetailSubscription: AnyCancellable?
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinDetail()
+    }
+    
+    func getCoinDetail() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/\(coin.id)?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false") else { return }
+        
+        coinDetailSubscription = NetworkingManager
+            .download(url: url)
+            .decode(type: CoinDetailModel.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoinDetail in
+                guard let self = self else { return }
+                self.coinDetail = returnedCoinDetail
+                self.coinDetailSubscription?.cancel()
+            })
+    }
+}
+
+
