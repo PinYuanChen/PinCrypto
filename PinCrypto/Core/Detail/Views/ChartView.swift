f720a4dd434005c74dc71427ee3557918ee14a79
Patrick <champion790713@gmail.com>
2022-12-29 14:47:19 +0800

    [Chart] add chart line and background

--- PinCrypto/Core/Detail/Views/ChartView.swift
+++ PinCrypto/Core/Detail/Views/ChartView.swift
@@ -7,24 +7,27 @@ import SwiftUI
 struct ChartView: View {
     
     let data: [Double]
+    let maxY: Double
+    let minY: Double
+    let lineColor: Color
     
     init(coin: CoinModel) {
-        self.data = coin.sparklineIn7D?.price ?? []
+        data = coin.sparklineIn7D?.price ?? []
+        maxY = data.max() ?? 0
+        minY = data.min() ?? 0
+        let priceChange = (data.last ?? 0) - (data.first ?? 0)
+        lineColor = priceChange > 0 ? .theme.green : .theme.red
     }
     
     var body: some View {
-        GeometryReader { geometry in
-            Path { path in
-                for index in data.indices {
-                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
-                    
-                    if index == 0 {
-                        path.move(to: CGPoint(x: 0, y: 0))
-                    }
-                    path.addLine(to: CGPoint(x: xPosition, y: 0))
-                }
-            }
-            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        VStack {
+            chartView
+                .frame(height: 200)
+                .background(chartBackground)
+                .overlay (
+                    chartYAxis
+                    , alignment: .leading
+                )
         }
     }
 }
@@ -32,5 +35,50 @@ struct ChartView: View {
 struct ChartView_Previews: PreviewProvider {
     static var previews: some View {
         ChartView(coin: dev.coin)
+    }
+}
+
+extension ChartView {
+    var chartView: some View {
+        GeometryReader { geometry in
+            Path { path in
+                for index in data.indices {
+                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+                    
+                    let yAxis = maxY - minY
+                    
+                    // reverse y position cuz 0 is at the top
+                    let yPosition = (1 - CGFloat((data[index] - minY) / yAxis)) * geometry.size.height
+                    
+                    
+                    if index == 0 {
+                        path.move(to: CGPoint(x: xPosition, y: yPosition))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+                }
+            }
+            .stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+        }
+    }
+    
+    var chartBackground: some View {
+        VStack {
+            Divider()
+            Spacer()
+            Divider()
+            Spacer()
+            Divider()
+            Spacer()
+        }
+    }
+    
+    var chartYAxis: some View {
+        VStack {
+            Text(maxY.formattedWithAbbreviations())
+            Spacer()
+            Text(((maxY + minY) / 2).formattedWithAbbreviations())
+            Spacer()
+            Text(minY.formattedWithAbbreviations())
+        }
     }
 }
