a4a72877d7999004a84b941809ba641aa22ed27d
Patrick <champion790713@gmail.com>
2022-12-29 16:00:24 +0800

    [Settings] create settings view

---
+++ PinCrypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,34 @@
+//
+// Created on 2022/12/29.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Hi"), footer: Text("Footer")) {
+                    Text("Hi")
+                    Text("Hi")
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XMarkButton(presentationMode: presentationMode)
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
